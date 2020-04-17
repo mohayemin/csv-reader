0d3c8c1e520f5be9b2d34e75e03f8a7ed2d53ac9
Mohayeminul Islam <mohayeminul.islam@gmail.com>
2020-04-17 16:12:30 +0600

    implement the demo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import * as csv from "csvtojson";
+
 
 @Component({
   selector: 'csvr-root',
   templateUrl: './app.component.html',
-  styles: []
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'csv-reader';
+  json: any[];
+  filename: string;
+  constructor() { }
+
+  fileChange(event: Event) {
+    const file = (event.target as HTMLInputElement).files[0];
+    this.filename = file.name;
+    file.text().then(content => {
+      csv().fromString(content).then(json => {
+        this.json = json;
+      });
+    })
+  }
 }
